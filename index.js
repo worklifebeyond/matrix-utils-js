cc07920911113432334cd9f8057bb07637081419
hynra <hynra@yahoo.com>
2020-06-23 14:59:08 +0700

    add debug mode

--- index.js
+++ index.js
@@ -6,10 +6,16 @@ module.exports = class MatrixUtils {
     constructor(params) {
         this.identifier = params.identity;
         this.baseUrl = params.baseUrl;
+        this.debug = params.debug || false;
+
+        if(this.debug){
+            console.log('[Matrix Utils] initiate with parameters: ', params);
+        }
 
         const lastChar = this.baseUrl[this.baseUrl.length - 1];
         if (lastChar === '/') {
-            this.baseUrl = this.baseUrl.charAt(this.baseUrl.length - 1);
+            this.baseUrl = this.baseUrl.slice(0, -1);
+            if(this.debug) console.log('[Matrix Utils] base url contains slash, remove it: ', this.baseUrl);
         }
 
         this.config = {
@@ -20,14 +26,17 @@ module.exports = class MatrixUtils {
     }
 
     async signInUser(params) {
+        if(this.debug) console.log('[Matrix Utils] sign in user: ', params.id);
         try {
             let data = {
                 userId: params.id,
                 password: params.password
             };
             const {data: response} = await axios.post(`${this.baseUrl}/users/token`, data, this.config);
+            if(this.debug) console.log('[Matrix Utils] sign in success: ', params.id);
             return response;
         } catch (e) {
+            if(this.debug) console.log('[Matrix Utils] sign in failed: ', e);
             throw e;
         }
     };
